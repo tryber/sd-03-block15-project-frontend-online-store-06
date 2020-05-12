0ba1410cac84b1554c98492a0b668a7a2669ac56
Mateus Moreira <ashuraw@hotmail.com>
2020-05-12 16:22:33 -0300

    implementando a API

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,11 @@
 export async function getCategories() {
-  const api = await fetch(`https://api.mercadolibre.com/sites/MLB/categories`);
+  const api = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
   const response = await api.json();
   return response;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const api = await fetch(
-    'https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}'
-  );
+  const api = await fetch('https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}');
   const response = await api.json();
   return response;
 }
