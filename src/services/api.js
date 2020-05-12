8df5581ff5d3fed5cf7ca49917a8fdce3337bcdd
Mateus Moreira <ashuraw@hotmail.com>
2020-05-12 16:27:21 -0300

    implementando a API

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ export async function getCategories() {
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const api = await fetch('https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}');
+  const api = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
   const response = await api.json();
   return response;
 }
