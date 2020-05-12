0c88c44e18adac3f77f2af766fde147219c3a436
leonardodamasceno8 <leonardodamasceno8@users>
2020-05-12 17:57:56 -0300

    Primeiro comit Cart

---
+++ src/componentes/cart.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Cart extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            empty: true,
+            cart: [],
+        };
+    }
+
+    render() {
+        const { empty } = this.state;
+
+        return (
+        <div className='cart'>
+            <img
+                data-testid="shopping-cart-button"
+                src="../images/carrinho.png"
+                alt="carrinho de compras"
+            />
+            {
+                empty ? <div className='Vazio'>
+                            <img src='../images/box.png' alt='Caixa vazia'/>
+                            <h3> Caixa vazia </h3>
+                        </div> : 1
+            }
+        </div>)
+    }
+}
+
+export default Cart;
