b3c7982aa5bdf45420a05d2e869281ba08fcb605
Gustaff <gustavohmfigueiredo@gmail.com>
2020-05-13 18:31:06 -0300

    Link de Cart dentro deProductLibrary

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Switch, Route, Link, BrowserRouter as Router } from 'react-router-dom';
-import Cart from './componentes/cart';
-import CartImg from './images/carrinho.png';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+
+import Cart from './Components/Cart';
 import ProductLibrary from './Components/ProductLibrary';
 
 function App() {
@@ -11,10 +11,6 @@ function App() {
         <Switch>
           <Route path="/cart" component={Cart} />
           <Route exact path="/" component={ProductLibrary} />
-            <Link to="/cart" data-testid="shopping-cart-button" >
-              <img src={CartImg} alt="cart-button" />
-            </Link>
-          </Route>
         </Switch>
       </Router>
     </div>
