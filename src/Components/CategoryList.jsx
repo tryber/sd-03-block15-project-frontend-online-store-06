6a8d649660efa28aa4b6cc4ee3246d8e5ae79358
Gustaff <gustavohmfigueiredo@gmail.com>
2020-05-15 00:10:44 -0300

    category change para componente funcional

--- src/Components/CategoryList.jsx
+++ src/Components/CategoryList.jsx
@@ -1,33 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class CategoryList extends React.Component {
-  render() {
-    const { categories, onCategoryChange, selectCategory } = this.props;
-    return (
-      <div>
-        <ul>
-          <h1>Category List</h1>
-          {categories.map(({ id, name }) => (
-            <li key={id}>
-              <label htmlFor={id}>
-                <input
-                  data-testid="category"
-                  type="radio"
-                  id={id}
-                  value={id}
-                  name="categories"
-                  checked={selectCategory === id}
-                  onChange={onCategoryChange}
-                />
-                {name}
-              </label>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+function CategoryList(props) {
+  const { categories, onCategoryChange, selectCategory } = props;
+  return (
+    <div>
+      <ul>
+        <h1>Category List</h1>
+        {categories.map(({ id, name }) => (
+          <li key={id}>
+            <label htmlFor={id}>
+              <input
+                data-testid="category"
+                type="radio"
+                id={id}
+                value={id}
+                name="categories"
+                checked={selectCategory === id}
+                onChange={onCategoryChange}
+              />
+              {name}
+            </label>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 CategoryList.propTypes = {
